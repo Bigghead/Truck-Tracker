bfa4e3accd7438048288807f036897e2e40466e6
Bigghead <shavenchew@yahoo.com>
2018-05-10 14:14:55 -0700

    allow react requests for server

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const PORT       = process.env.PORT || 9009;
 const bodyParser = require('body-parser');
 const mongoose   = require('mongoose');
+const cors       = require('cors');
 const express    = require('express');
 const Jobs       = require('./models/job.js');
 const Trucks     = require('./models/truck.js');
+const truckRoutes = require('./routes/truck');
 
 const app        = express();
 
@@ -37,8 +39,13 @@ seedDB();
 
 
 // ===== MIDDLEWARES ===== //
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// ===== ROUTES ===== //
+app.use( '/api', truckRoutes )
 
 
 
